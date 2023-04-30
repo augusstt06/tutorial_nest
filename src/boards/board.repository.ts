d3148ddeb66b9cd69d4f9e266904e784d84faca5
augusstt06 <augusstt06@gmail.com>
2023-04-30 19:09:38 +0900

    EntityRepository is deprecated - issue #1

---
+++ src/boards/board.repository.ts
@@ -0,0 +1,10 @@
+// 레포지토리 생성하기
+// 레포지토리는 엔터티 객체와 함께 작동하며 엔티티 찾기, 삽입, 업데이트, 삭제 등을 처리한다.
+
+import { EntityRepository, Repository } from 'typeorm';
+import { Board } from './board.entitiy';
+import { CustomRepository } from './decorator/typeorm-ex.decorator';
+
+// @CustomRepository(Board)
+@EntityRepository(Board)
+export class BoardRepository extends Repository<Board> {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { TypeOrmExModule } from './decorator/typeorm-ex.module';
 
 @Module({
+  imports: [TypeOrmExModule.forCustomRepository([])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
