52e334254c28ad6174b3c9e85f7058f4114eb4ce
augusstt06 <augusstt06@gmail.com>
2023-05-01 06:31:53 +0900

    Custom Repository - issue #1

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-// import { TypeOrmExModule } from './decorator/typeorm-ex.module';
-// import { BoardRepository } from './board.repository';
+import { TypeOrmExModule } from './decorator/typeorm-ex.module';
+import { BoardRepository } from './board.repository';
 
 @Module({
-  // imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
