61827934cc95f1634f3b83f92105671f2fd0846d
augusstt06 <augusstt06@gmail.com>
2023-05-01 06:31:23 +0900

    Comment

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,10 +1,9 @@
 // 레포지토리 생성하기
 // 레포지토리는 엔터티 객체와 함께 작동하며 엔티티 찾기, 삽입, 업데이트, 삭제 등을 처리한다.
 
-import { EntityRepository, Repository } from 'typeorm';
-import { Board } from './board.entitiy';
-import { CustomRepository } from './decorator/typeorm-ex.decorator';
+// import { Repository } from 'typeorm';
+// import { Board } from './board.entitiy';
+// import { CustomRepository } from './decorator/typeorm-ex.decorator';
 
 // @CustomRepository(Board)
-@EntityRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+// export class BoardRepository extends Repository<Board> {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-import { TypeOrmExModule } from './decorator/typeorm-ex.module';
+// import { TypeOrmExModule } from './decorator/typeorm-ex.module';
+// import { BoardRepository } from './board.repository';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([])],
+  // imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
