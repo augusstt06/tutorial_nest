1d1c7607d1b8dd8c3913e2daabd30b5a236b83ad
augusstt06 <augusstt06@gmail.com>
2023-04-30 18:49:58 +0900

    Create Board Entity

---
+++ src/boards/board.entitiy.ts
@@ -0,0 +1,18 @@
+// 엔티티설정
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './boards.model';
+
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
+}
